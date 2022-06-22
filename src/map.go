7bdbe282ce014da15b334e3d31ec0bd0c099d523
Tyler Britten <1933680+tybritten@users.noreply.github.com>
2022-06-21 20:53:22 -0400

    revert map

--- src/map.go
+++ src/map.go
@@ -80,7 +80,6 @@ func main() {
 	for word, count := range wordMap {
 		newPath := filepath.Join(outputDir,os.Getenv("PACH_DATUM_ID"))
 		os.MkdirAll(newPath, os.ModePerm)
-		word += ".txt"
 		if err := ioutil.WriteFile(filepath.Join(newPath,word), []byte(strconv.Itoa(count)+"\n"), 0644); err != nil {
 			log.Fatal(err)
 		}
